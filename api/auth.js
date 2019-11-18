dd29429acdfb0fb1b253094a3aa829f4358555ac
euphokumiko <h.k.h94539@gmail.com>
2019-11-18 14:19:04 +0800

    update

--- api/auth.js
+++ api/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const sql = require('mysql');
+const sql = require('mysql2');
 
 const connection = sql.createPool({
     host: 'localhost',
--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const router = express.Router();
 
-const sql = require('mysql');
+const sql = require('mysql2/promise');
 const jwt = require('jsonwebtoken');
 
 const auth = require('../api/auth');
@@ -14,6 +14,17 @@ const connection = sql.createPool({
     database: 'kumiko'
 });
 
+router.use(cors());
+
+router.post('/adminlogin', async (req, res, next) => {
+    await connection.execute('SELECT * FROM `user` WHERE `username` = ? AND `IsAdmin` = 1',[req.body.account], (err, result) => {
+        if(err) throw err;
+        if(result[0].password == req.body.pwd){
+            
+        }
+    });
+});
+
 router.post('/adduser', async (req, res, next) => {
     let temp = await auth.auth(req.header('Authorization'));
     
@@ -23,3 +34,5 @@ router.post('/adduser', async (req, res, next) => {
 
 
 module.exports = router;
+
+//pasi無更新?
